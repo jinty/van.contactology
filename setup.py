5107d3740fd4be0676ba8b16c2995f667c08a6b4
Brian Sutherland <brian@vanguardistas.net>
2011-04-06 09:38:39 +0200

    Use __version__ from __init__.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 import os
+import re
 from setuptools import setup, find_packages
 
+_here = os.path.dirname(__file__)
+_init = os.path.join(_here, 'van', 'contactology', '__init__.py')
+_init = open(_init, 'r').read()
+
+VERSION = re.search(r'^__version__ = "(.*)"', _init, re.MULTILINE).group(1)
+
 setup(name="van.contactology",
-      version='1.0dev',
+      version=VERSION,
       packages=find_packages(),
       description="Contactology API for Twisted",
       namespace_packages=["van"],
